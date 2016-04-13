ea1b18ad505523981594fde25a3358a6869f1174
WorkPC <WorkPC@DESKTOP-SF1T0BB>
2016-04-13 04:14:59 +0300

    Add project files.

---
+++ ConsoleApplication1/Kosti.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Kosti
+    {
+        private List<int> arvot;
+
+        public Kosti()
+        {
+            arvot = new List<int>();
+        }
+        public void AlkuTila()
+        {
+            if (arvot.Count == 0)
+            {
+                Console.WriteLine("Kosti on alkutilassa");
+            }
+            else
+            {
+                Console.WriteLine("Kosti ei ole alkutilassa, kosti sisältää {0} kappaletta arvoja", arvot.Count);
+            }
+
+        }
+        public void Nollaa()
+        {
+            arvot.Clear();
+            Console.WriteLine("Kosti nollattu");
+        }
+        public void SyotaArvo(int arvo)
+        {
+            Console.WriteLine("Kosti vastaanotti arvon: {0}", arvo);
+            arvot.Add(arvo);
+        }
+        public void TulostaMinMax()
+        {
+            Console.WriteLine("Kosti minimi: {0}", arvot.Min());
+            Console.WriteLine("Kosti maksimi: {0}", arvot.Max());
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kosti laite = new Kosti();
+
+            laite.AlkuTila();
+            laite.SyotaArvo(15);
+            laite.SyotaArvo(17);
+            laite.SyotaArvo(12);
+            laite.TulostaMinMax();
+            laite.Nollaa();
+
+            Console.ReadLine();
+
+        }
+    }
+}
